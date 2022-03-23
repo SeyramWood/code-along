239461a17e4c25488cdb7456f0b0ed81d423c627
SeyramWood <prikahaeyram@gmail.com>
2022-03-23 14:25:14 +0000

    React Component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
+import React from "react";
 import "./App.css";
 import { ProfileCard } from "./ProfileCard";
-import writers from "./writers";
 
 function App() {
+  const [writers, setWriters] = React.useState([]);
+
+  React.useEffect(() => {
+    const getWriters = async () => {
+      const res = await fetch("./writers.json");
+      const data = await res.json();
+      setWriters(data);
+    };
+    getWriters();
+  }, []);
+
   return (
     <div className="App">
       <h1>Writer Profiles.</h1>
