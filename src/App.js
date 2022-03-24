9b445a99a2133b4758fcfb0aa6f33e8e3a51bcf3
SeyramWood <prikahaeyram@gmail.com>
2022-03-24 13:49:02 +0000

    React Component 4

--- src/App.js
+++ src/App.js
@@ -1,27 +1,67 @@
 import React from "react";
 import "./App.css";
-import { ProfileCard } from "./ProfileCard";
+import ProfileCard from "./ProfileCard";
 
 function App() {
-  const [writers, setWriters] = React.useState([]);
+  const [data, setData] = React.useState({
+    writers: [],
+    loading: false,
+  });
 
+  const handleClick = () => {
+    setData((data) => ({
+      ...data,
+      loading: true,
+    }));
+    setTimeout(() => {
+      const getWriters = async () => {
+        const res = await fetch("./writers.json");
+        const results = await res.json();
+        setData((data) => ({
+          ...data,
+          writers: results,
+          loading: false,
+        }));
+      };
+      getWriters();
+    }, 2000);
+  };
   React.useEffect(() => {
-    const getWriters = async () => {
-      const res = await fetch("./writers.json");
-      const data = await res.json();
-      setWriters(data);
-    };
-    getWriters();
+    handleClick();
   }, []);
 
+  if (data.loading) {
+    return (
+      <div>
+        <h1>Writers Profile</h1>
+        <div className="container">
+          <div className="card action">
+            <div className="infoText">Loading...</div>
+          </div>
+        </div>
+      </div>
+    );
+  }
   return (
     <div className="App">
+      <button type="button" onClick={handleClick}>
+        Fetch Data
+      </button>
       <h1>Writer Profiles.</h1>
-      <div className="container">
-        {writers.map((writer) => (
-          <ProfileCard writer={writer} key={writer.id} />
-        ))}
-      </div>
+      {data.writers.length > 0 && (
+        <div className="container">
+          {data.writers.map((writer) => (
+            <ProfileCard writer={writer} key={writer.id} />
+          ))}
+        </div>
+      )}
+      {data.writers.length === 0 && (
+        <div className="container">
+          <div className="card action">
+            <div className="infoText">Ooops...no writer profile found.</div>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
