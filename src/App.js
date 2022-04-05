b8baf595820ad8cee1e59167073f2cbd0553bb6f
SeyramWood <prikahaeyram@gmail.com>
2022-04-05 14:13:35 +0000

    React Component 9

--- src/App.js
+++ src/App.js
@@ -1,28 +1,25 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import useLocalStorage from "./hooks/useLocalStorage";
-import ProfileForm from "./components/ProfileForm";
-import ProfileCard from "./components/ProfileCard";
+import Blog from "./pages/Blog";
+import BlogList from "./pages/BlogList";
+import About from "./pages/About";
+import Home from "./pages/Home";
+import Navbar from "./Navbar/Navbar";
+import BlogDetail from "./pages/BlogDetail";
 
 function App() {
-  const [profiles, setProfiles] = useLocalStorage("profiles", []);
-  const updateProfiles = (profile) => {
-    let arr = profiles;
-    arr.push(profile);
-    setProfiles([...arr]);
-  };
-
   return (
-    <div className="app">
-      <h1> Profile Maker </h1>
-      <div>
-        <ProfileForm submit={updateProfiles} />
-        <hr />
-        <div className="list">
-          {profiles.map((person, index) => (
-            <ProfileCard key={index} writer={person} />
-          ))}
-        </div>
-      </div>
-    </div>
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="about" element={<About />} />
+        <Route path="blog" element={<Blog />}>
+          <Route path="" element={<BlogList />} />
+          <Route path=":blog" element={<BlogDetail />} />
+        </Route>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
